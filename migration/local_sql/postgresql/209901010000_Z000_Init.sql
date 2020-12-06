050cb53b1b8cd5977b8061afbe08d9e7f4de2638
Ken Longnan <ken.longnan@gmail.com>
2020-12-06 18:07:15 +0800

    ...

--- migration/local_sql/postgresql/209901010000_Z000_Init.sql
+++ migration/local_sql/postgresql/209901010000_Z000_Init.sql
@@ -1,34 +1,23 @@
 ---- Update ad_system to KylinBizSuite
-UPDATE ad_system SET lastbuildinfo = '7.1.0.latest', name = 'KylinBizSuite' WHERE ad_system_id = 0;
+UPDATE ad_system SET lastbuildinfo = '0.0.0', name = 'KylinBizSuite' WHERE ad_system_id = 0;
 
 ---- Setup APPLICATION_MAIN_VERSION
 INSERT INTO ad_sysconfig(
 	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
-	VALUES (999999, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'APPLICATION_MAIN_VERSION', '7.1.0.latest', 'Application Main Version', 'D', 'S', 'c44e2ee5-68b5-46cb-adef-98fb3511269c');
+	VALUES (999999, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'APPLICATION_MAIN_VERSION', '0.0.0', 'Application Main Version', 'D', 'S', 'c44e2ee5-68b5-46cb-adef-98fb3511269c');
 	
 ---- Setup APPLICATION_IMPLEMENTATION_VENDOR
 INSERT INTO ad_sysconfig(
 	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
 	VALUES (999998, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'APPLICATION_IMPLEMENTATION_VENDOR', 'Supported by KSYS', 'Supported by KSYS', 'D', 'S', '811c8b1f-544a-483f-a4bf-b281490e1b72');
 
----- Setup ZK_BROWSER_ICON
-INSERT INTO ad_sysconfig(
-	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
-	VALUES (999997, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'ZK_BROWSER_ICON', '/theme/kbs/images/icon.png', 'ZK_BROWSER_ICON', 'D', 'S', '3ce21ae8-f23b-4c82-865f-8e9392970504');
----- Setup ZK_LOGO_SMALL
-INSERT INTO ad_sysconfig(
-	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
-	VALUES (999996, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'ZK_LOGO_SMALL', '/theme/kbs/images/header-logo.png', 'ZK_LOGO_SMALL', 'D', 'S', '4ec69e43-44d8-40eb-ba35-dac9456f1c99');
----- Setup ZK_LOGO_LARGE
-INSERT INTO ad_sysconfig(
-	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
-	VALUES (999995, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'ZK_LOGO_LARGE', '/theme/kbs/images/login-logo.png', 'ZK_LOGO_LARGE', 'D', 'S', '18f071b8-9dd8-4005-a596-9729417db897');
-
 ---- Setup DB_READ_REPLICA_URLS
 --INSERT INTO ad_sysconfig(
 --	ad_sysconfig_id, ad_client_id, ad_org_id, created, updated, createdby, updatedby, isactive, name, value, description, entitytype, configurationlevel, ad_sysconfig_uu)
 --	VALUES (999994, 0, 0, '2020-01-01', '2020-01-01', 100, 100, 'Y', 'DB_READ_REPLICA_URLS', 'jdbc:postgresql://kbs-pgslave:5432/idempiere?encoding=unicode', 'DB_READ_REPLICA_URLS', 'D', 'S', '2871cc29-7691-45a7-a5e8-8fefded48734');
 
+---- Setup ZK_THEME to kbs
+update ad_sysconfig set value = 'kbs' where name = 'ZK_THEME';
 
 ---- Setup ALogin_ShowDate to N
 update ad_sysconfig set value = 'N' where name = 'ALogin_ShowDate';
@@ -42,8 +31,6 @@ update ad_sysconfig set Value = 'Y' where name = 'ZK_THEME_USE_FONT_ICON_FOR_IMA
 update ad_sysconfig set value = 20480 where name = 'ZK_MAX_UPLOAD_SIZE';
 ---- Setup "ZK_GRID_AFTER_FIND"
 update ad_sysconfig set value = 'Y' where name = 'ZK_GRID_AFTER_FIND';
----- Setup ZK_THEME to kbs
-update ad_sysconfig set value = 'kbs' where name = 'ZK_THEME';
 ---- Setup "Forgot My Password" 
 update ad_sysconfig set value = 'N' where name = 'LOGIN_SHOW_RESETPASSWORD';
 
--- migration/local_sql/postgresql/DB_Version_20200902.sql
+++ migration/local_sql/postgresql/DB_Version_20200902.sql
@@ -1,8 +1,8 @@
 ---- Update lastbuildinfo of KylinBizSuite in ad_system
---UPDATE ad_system SET lastbuildinfo = '7.1.0.latest' WHERE ad_system_id = 0;
+UPDATE ad_system SET lastbuildinfo = '8.1.0.latest' WHERE ad_system_id = 0;
 
 ---- Update APPLICATION_MAIN_VERSION in ad_sysconfig
---update ad_sysconfig set value = '7.1.0.latest' where ad_sysconfig_id = 999999;
+update ad_sysconfig set value = '8.1.0.latest' where ad_sysconfig_id = 999999;
 	
 ---- Register SQL
 SELECT register_migration_script('DB_Version_20200902.sql') FROM dual;
